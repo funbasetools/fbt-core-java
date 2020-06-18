4bb87cec87ad01acbeb6d5d582b1590c0e5899d3
Roberto Martinez <hdkrus@gmail.com>
2020-06-18 19:02:41 -0400

    add Try and throwing functions (#15)

---
+++ src/main/java/com/funbasetools/ThrowingSupplier.java
@@ -0,0 +1,7 @@
+package com.funbasetools;
+
+@FunctionalInterface
+public interface ThrowingSupplier<T, E extends Exception> {
+
+    T get() throws E;
+}
---
+++ src/main/java/com/funbasetools/Try.java
@@ -0,0 +1,110 @@
+package com.funbasetools;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public abstract class Try<T> {
+
+    public static Try<Object> of(final Runnable runnable) {
+        return of(() -> {
+            runnable.run();
+            return new Object();
+        });
+    }
+
+    public static <T> Try<T> of(final ThrowingSupplier<T, ? extends  Exception> supplier) {
+        try {
+            final T result = supplier.get();
+            return success(result);
+        }
+        catch (final Exception ex) {
+            return failure(ex);
+        }
+    }
+
+    public static <T> Try<T> success(final T result) {
+        return new Success<>(result);
+    }
+
+    public static <T, E extends Exception> Try<T> failure(final E ex) {
+        return new Failure<>(ex);
+    }
+
+    public abstract Optional<T> toOptional();
+
+    public abstract Optional<Exception> toFailureOptional();
+
+    public abstract boolean isSuccess();
+
+    public boolean isFailure() {
+        return !isSuccess();
+    }
+
+    public <R> Try<R> flatMap(final Function<T, Try<R>> f) {
+        return toOptional()
+            .map(f)
+            .orElseGet(() ->
+                failure(
+                    toFailureOptional().orElse(new Exception("Unknown error"))
+                )
+            );
+    }
+
+    public <R> Try<R> map(final ThrowingFunction<T, R, ? extends Exception> f) {
+        return toOptional()
+            .map(result -> Try.of(() -> f.apply(result)))
+            .orElseGet(() ->
+                failure(
+                    toFailureOptional().orElse(new Exception("Unknown error"))
+                )
+            );
+    }
+
+    private static final class Success<T> extends Try<T> {
+
+        private final T result;
+
+        private Success(T result) {
+            this.result = result;
+        }
+
+        @Override
+        public Optional<T> toOptional() {
+            return Optional.ofNullable(result);
+        }
+
+        @Override
+        public Optional<Exception> toFailureOptional() {
+            return Optional.empty();
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return true;
+        }
+    }
+
+    private static final class Failure<T> extends Try<T> {
+
+        private final Exception exception;
+
+        private Failure(Exception exception) {
+            this.exception = exception;
+        }
+
+        @Override
+        public Optional<T> toOptional() {
+            return Optional.empty();
+        }
+
+        @Override
+        public Optional<Exception> toFailureOptional() {
+            return Optional.of(exception);
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return false;
+        }
+    }
+}
