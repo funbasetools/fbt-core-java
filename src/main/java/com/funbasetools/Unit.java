a1f9385a2d5e9c985d1bc00248244b9a5f3b0b94
Roberto Martinez <hdkrus@gmail.com>
2020-06-20 18:13:53 -0400

    improve unit

--- src/main/java/com/funbasetools/Unit.java
+++ src/main/java/com/funbasetools/Unit.java
@@ -1,5 +1,7 @@
 package com.funbasetools;
 
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import java.util.function.Supplier;
 
 public final class Unit {
@@ -18,6 +20,23 @@ public final class Unit {
             runnable.run();
             return value;
         };
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(Unit.class)
+            .build();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Unit;
+    }
+
+    @Override
+    public String toString() {
+        return "unit";
     }
 
     private Unit() {
