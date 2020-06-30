32d3833a59670ba880340d54aaa3164fbf63fc9c
Roberto Martinez <hdkrus@gmail.com>
2020-06-30 17:06:51 -0400

    add local system resource accessories

---
+++ src/main/java/com/funbasetools/io/ResourceStream.java
@@ -0,0 +1,35 @@
+package com.funbasetools.io;
+
+import com.funbasetools.ThrowingRunnable;
+import com.funbasetools.collections.Stream;
+import com.funbasetools.collections.Streams;
+import java.io.Closeable;
+import java.io.IOException;
+
+public interface ResourceStream<T> extends Closeable {
+
+    Stream<T> getStream();
+
+    static <T> ResourceStream<T> of(final Stream<T> stream, final ThrowingRunnable<IOException> close) {
+        return new ResourceStream<>() {
+
+            @Override
+            public Stream<T> getStream() {
+                return stream;
+            }
+
+            @Override
+            public void close() throws IOException {
+                close.run();
+            }
+        };
+    }
+
+    static <T> ResourceStream<T> of(final Stream<T> stream) {
+        return of(stream, () -> {});
+    }
+
+    static <T> ResourceStream<T> empty() {
+        return of(Streams.emptyStream());
+    }
+}
