c09fb9ca08d5ef3363f23234c42da33457e2a053
Roberto Martinez <hdkrus@gmail.com>
2020-07-09 18:46:33 -0400

    fix encode api

--- src/main/java/com/funbasetools/codecs/BinaryEncoder.java
+++ src/main/java/com/funbasetools/codecs/BinaryEncoder.java
@@ -2,7 +2,6 @@ package com.funbasetools.codecs;
 
 import static com.funbasetools.io.IOUtils.toInputStream;
 
-import com.funbasetools.ShouldNotReachThisPointException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,13 +9,8 @@ import java.io.OutputStream;
 public interface BinaryEncoder extends ToBinaryEncoder<byte[]> {
 
     @Override
-    default void encodeTo(final byte[] source, final OutputStream outputStream) {
-        try {
-            encode(toInputStream(source), outputStream);
-        }
-        catch (IOException ex) {
-            throw new ShouldNotReachThisPointException(ex);
-        }
+    default void encodeTo(final byte[] source, final OutputStream outputStream) throws IOException {
+        encode(toInputStream(source), outputStream);
     }
 
     void encode(final InputStream inputStream, final OutputStream outputStream) throws IOException;
