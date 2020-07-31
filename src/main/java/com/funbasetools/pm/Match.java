aa67e9bc80a1014f9ba6ece9ffc6eaec06429cbb
Roberto Martinez <hdkrus@gmail.com>
2020-07-31 09:07:37 -0400

    add when for pairs

--- src/main/java/com/funbasetools/pm/Match.java
+++ src/main/java/com/funbasetools/pm/Match.java
@@ -2,11 +2,16 @@ package com.funbasetools.pm;
 
 import com.funbasetools.pm.statements.ContinuingStatement;
 import com.funbasetools.pm.statements.MatchStatement;
+import org.apache.commons.lang3.tuple.Pair;
 
 public final class Match {
 
     public static <EXPR, R> MatchStatement<EXPR, R> when(final EXPR expr) {
         return new ContinuingStatement<>(expr);
+    }
+
+    public static <A, B, R> MatchStatement<Pair<A, B>, R> when(final A left, final B right) {
+        return when(Pair.of(left, right));
     }
 
     private Match() {
